0fb11b2d51fa741ec53281770f79b18cd0ff9cf6
pdr64 <pdr64@cornell.edu>
2019-04-23 16:09:43 -0400

    initial push

---
+++ Lab_5/process.h
@@ -0,0 +1,19 @@
+#ifndef __PROCESS_H_INCLUDED__
+#define __PROCESS_H_INCLUDED__
+
+//We make use of a header file here so that we can have access
+//to the global variables we want as well as the global function
+//push_tail_process.
+
+#include "3140_concur.h"
+#include <fsl_device_registers.h>
+#include "shared_structs.h"
+#include <stdlib.h>
+
+extern process_t * current_process;
+extern process_t * process_queue;
+extern process_t * process_tail;
+
+void push_tail_process(process_t *proc);
+
+#endif
---
+++ Lab_5/shared_structs.h
@@ -0,0 +1,36 @@
+#ifndef __SHARED_STRUCTS_H__
+#define __SHARED_STRUCTS_H__
+#include "3140_concur.h"
+#include <fsl_device_registers.h>
+#include "shared_structs.h"
+#include <stdlib.h>
+/** Implement your structs here */
+
+/**
+ * This structure holds the process structure information
+ */
+struct process_state {
+	unsigned int *sp;
+	unsigned int *orig_sp;
+	int n;
+	process_t *next;
+	int blocked;			
+};
+
+/**
+ * This defines the lock structure
+ */
+typedef struct lock_state {
+	int held;
+	process_t * blocked_queue;
+	process_t * blocked_queue_end;
+} lock_t;
+
+/**
+ * This defines the conditional variable structure
+ */
+typedef struct cond_var {
+
+} cond_t;
+
+#endif
